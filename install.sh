c640dc97f1addb5a7631a808bbf7369c73d7fcd5
t4skforce <t4skforce1337@gmail.com>
2019-01-19 22:19:14 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OUTFILE=/tmp/install.exp
+
+function auto_accept {
+cat <<EOF >>$OUTFILE
+spawn $1;
+expect {
+  "y/N" { exp_send "y\r" ; exp_continue }
+  eof
+}
+EOF
+}
+
+cat <<EOF >$OUTFILE
+#!/usr/bin/expect
+set timeout -1;
+EOF
+
+auto_accept 'sdkmanager --licenses'
+auto_accept 'sdkmanager "tools"'
+auto_accept 'sdkmanager "platform-tools"'
+auto_accept 'sdkmanager "extras;android;m2repository"'
+auto_accept 'sdkmanager "extras;google;m2repository"'
+
+for VER in $(sdkmanager --list 2>/dev/null | cut -d'|' -f1 | awk '{$1=$1};1' | grep -v '\-rc' | grep 'build-tools\;' | sort -r | head -n 1); do
+	auto_accept "sdkmanager \"$VER\""
+done
+
+for VER in $(sdkmanager --list 2>/dev/null | cut -d'|' -f1 | awk '{$1=$1};1' | grep -v '\-rc' | grep 'platforms\;' | cut -d'-' -f2 | sort -nr | head -n 6); do
+	auto_accept "sdkmanager \"platforms;android-$VER\""
+done
+
+cat $OUTFILE
+expect $OUTFILE
